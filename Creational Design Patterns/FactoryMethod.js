92c90b1f3b5362088f28c5c9d2a118cc0a9264b3
Mohammad Suhail <mohd.suhail100@gmail.com>
2025-04-07 22:08:46 +0100

    Add: Factory Method

--- Creational Design Patterns/FactoryMethod.js
+++ Creational Design Patterns/FactoryMethod.js
@@ -16,3 +16,17 @@ class EmailNotification extends Notification {
       console.log(`Sending SMS: ${message}`);
     }
   }
+
+  class NotificationFactory {
+    static createNotification(notificationType) {
+      switch(notificationType){
+        case 'email': return new EmailNotification();
+        case 'sms': return new SMSNotification();
+        default: throw new Error('Unsupported type!');
+      }
+    }
+  }
+
+// Usage
+const notification = NotificationFactory.createNotification('email');
+notification.send('Welcome user!');
